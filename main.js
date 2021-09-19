286c7741a28772964c276b58cbfffa30a7c00d12
Antoine Dricot <antdricot@gmail.com>
2021-09-19 19:11:17 +0200

    update resolution displayed

--- main.js
+++ main.js
@@ -1,28 +1,40 @@
+// Hello
+const strName = varAsObj => Object.keys(varAsObj)[0] // https://stackoverflow.com/a/42791996
+
+function updateElements() {
+    let window_innerHeight = window.innerHeight;
+    let window_innerWidth = window.innerWidth;
+
+    let e0 = document.getElementById("window.innerHeight");
+    if (e0) {
+        e0.textContent = strName({ window_innerHeight }) + " = " + window_innerHeight;
+    }
+
+    let e1 = document.getElementById("window.innerWidth");
+    if (e1) {
+        e1.textContent = strName({ window_innerWidth }) + " = " + window_innerWidth;
+    }
+    requestAnimationFrame(updateElements);
+}
+
 function main() {
-    // // -- Canvas
-    // let mainCanvas = document.createElement("canvas");
-    // let actualHeight = window.innerHeight;
-    // mainCanvas.height = actualHeight;
-    // mainCanvas.width = actualHeight * 10 / 16;
-    // document.body.appendChild(mainCanvas);
+    const lineBreak = document.createElement('br');
 
-    // // -- Control
-    // document.addEventListener("keydown", keyDownHandler, false);
-    // document.addEventListener("keyup", keyUpHandler, false);
-    // document.addEventListener("touchstart", touchDownHandler, false);
-    // document.addEventListener("touchend", touchUpDownHandler, false);
+    let div = document.createElement("div");
 
-    // // Run
-    // let mainGame = new Game(mainCanvas);
-    // mainGame.initialize();
-    // mainGame.run();
-    let paragraph = document.createElement("p");
-    paragraph.textContent = "Ceci est un test, pas de panique."
-    document.body.appendChild(paragraph);
+    let p = document.createElement("p");
+    p.id = "window.innerHeight";
+    div.appendChild(p);
+
+    p = document.createElement("p");
+    p.id = "window.innerWidth";
+    div.appendChild(p);
+
+    document.body.appendChild(div);
+
+    updateElements();
 }
 
 main();
 
-// TODO: create text
-// TODO: add resolution
-// TODO: update resolution on resize
+// TODO: factorize
